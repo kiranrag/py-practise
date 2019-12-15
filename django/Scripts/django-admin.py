108e1dadd94817ff807b4505b03b428356137f60
kiranrag <kiranrag92@gmail.com>
2019-12-15 12:21:12 +0530

    django initialise

---
+++ django/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\rags\docume~1\python codes\udemy\practise codes\py-practise\django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
